dd36b1f9777c48dba6c705053fb3d6ad3400ccd7
Sihun Kim <skim585@asu.edu>
2023-01-09 13:50:03 -0700

    feat(main): add cors middleware package
    add CORS middleware package to handle access-control-allow-orign: *

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,35 @@
-import express, { Response } from "express";
+import express, { Response } from 'express'
+import cors from 'cors'
 
 // routes
-import { UserRouter } from "@/routes/user";
-import { AuthRouter } from "@/routes/auth";
-import { PostController } from "@/routes/post";
-import { CategoryRouter } from "@/routes/category";
-import { CommentRouter } from "@/routes/comment";
+import { UserRouter } from '@/routes/user'
+import { AuthRouter } from '@/routes/auth'
+import { PostController } from '@/routes/post'
+import { CategoryRouter } from '@/routes/category'
+import { CommentRouter } from '@/routes/comment'
 
 // initialize express app
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
-app.get("/", (_, res: Response) => {
-  res.json({ message: "POC-Cookbook-API listening through Lambda" });
-});
+app.get('/', (_, res: Response) => {
+  res.json({ message: 'POC-Cookbook-API listening through Lambda' })
+})
 
 // define routes
-app.use("/user", UserRouter);
-app.use("/auth", AuthRouter);
-app.use("/post", PostController);
-app.use("/category", CategoryRouter);
-app.use("/comment", CommentRouter);
+app.use('/user', UserRouter)
+app.use('/auth', AuthRouter)
+app.use('/post', PostController)
+app.use('/category', CategoryRouter)
+app.use('/comment', CommentRouter)
 
 app.use((_, res: Response) => {
   return res.status(404).json({
-    error: "Not Found",
-  });
-});
+    error: 'Not Found',
+  })
+})
 
-export { app };
+export { app }
